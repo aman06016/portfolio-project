9da3ca2c3cc6fe25973b61231e66cb086e034daa
Aman gupta <aman06016@gmail.com>
2020-05-01 18:22:50 +0530

    blog app is up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+
+    title=models.CharField(max_length=100)
+    date=models.DateField(auto_now=False,auto_now_add=False)
+    text=models.TextField()
+    image=models.ImageField(upload_to='images/')
